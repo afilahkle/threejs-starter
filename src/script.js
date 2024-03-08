1e8cca84474d1ae8cbac3b558ffa489b229a28d4
Omar Khalifa <elkhalifa002@gmail.com>
2024-03-08 06:09:31 +0200

    add shader

--- src/script.js
+++ src/script.js
@@ -7,6 +7,36 @@ const sizes = {
   height: window.innerHeight,
 };
 
+const mouse = {
+  x: 0,
+  y: 0,
+};
+
+const vertexShader = `
+  void main() {
+        gl_Position = vec4( position, 1.0 );
+    }
+`;
+
+const fragmentShader = `
+  uniform vec2 u_resolution;
+  uniform vec2 u_mouse;
+  uniform float u_time;
+
+  void main() {
+      vec2 st = gl_FragCoord.xy/u_resolution.xy;
+      gl_FragColor=vec4(st.x, st.y,0.0,1.0);
+  }
+`;
+
+const uniforms = {
+  u_time: { value: 1.0 },
+  u_mouse: { value: new THREE.Vector2() },
+  u_resolution: {
+    value: new THREE.Vector2(sizes.width, sizes.height),
+  },
+};
+
 const initThreeJsScene = (canvas) => {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
@@ -22,13 +52,17 @@ const initThreeJsScene = (canvas) => {
 
   camera.position.z = 5;
 
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshNormalMaterial();
-  const cube = new THREE.Mesh(geometry, material);
+  const geometry = new THREE.PlaneGeometry(2, 2);
+  const material = new THREE.ShaderMaterial({
+    uniforms,
+    vertexShader,
+    fragmentShader,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
 
   const controls = new OrbitControls(camera, canvas);
 
-  scene.add(cube);
+  scene.add(mesh);
 
   onresize = () => {
     sizes.width = window.innerWidth;
@@ -39,13 +73,22 @@ const initThreeJsScene = (canvas) => {
 
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    material.uniforms.u_resolution.value.x = renderer.domElement.width;
+    material.uniforms.u_resolution.value.y = renderer.domElement.height;
+    material.uniforms.u_mouse.value.x = mouse.x;
+    material.uniforms.u_mouse.value.y = mouse.y;
+  };
+
+  onmousemove = (e) => {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
   };
 
   const animate = () => {
     controls.update();
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    material.uniforms.u_time.value += new THREE.Clock().getDelta();
     renderer.render(scene, camera);
   };
 
